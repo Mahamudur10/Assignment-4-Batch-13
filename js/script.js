b58a130bedfea9ddff496d8c830b5e0ae5fa213c
Mahamudur10 <mahamudur649@gmail.com>
2026-02-23 21:00:58 +0600

    for display total number

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("connect script js")
+let Total = document.getElementById("total");
+let InterviewCount = document.getElementById("interviewCount");
+let RejectedCount = document.getElementById("rejectedCount");
+
+// total number display
+const allCardSection = document.getElementById("allCards");
+console.log(allCardSection.children.length);
+
+function calculatorCount(){
+    Total.innerText = allCardSection.children.length;
+}
+calculatorCount()
